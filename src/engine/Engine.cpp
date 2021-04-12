d2c2ed2abbab551fa2f98c1bf5e4da7a94616304
wimbleimble <wjt2001live@gmail.com>
2021-04-12 23:54:53 +0100

    minor changes, added complaints

--- src/engine/Engine.cpp
+++ src/engine/Engine.cpp
@@ -21,6 +21,16 @@ Engine::~Engine()
 
 int Engine::exec(State* entryState)
 {
+	// idea - state machine, that can be in two states - normal, or state change.
+	// or encapsulate this all in a function called 'shit' or something that i can
+	// return from.
+
+	// literally anything is better than the current deal.
+
+	// actually no i thought of a worse way of doing it and it involves exceptions
+
+	// oh god im actually entertaining the idea of doing the exception thing.
+
 	setState(entryState);
 
 	Uint64 currentTime{ SDL_GetPerformanceCounter() };
@@ -32,6 +42,12 @@ int Engine::exec(State* entryState)
 	_run = true;
 	while (_run)
 	{
+		// why
+		if (newState)
+		{
+			setState(newState);
+			newState = nullptr;
+		}
 		prevTime = currentTime;
 		currentTime = SDL_GetPerformanceCounter();
 
@@ -42,6 +58,10 @@ int Engine::exec(State* entryState)
 			);
 		while (SDL_PollEvent(&event))
 		{
+			// please no
+			if (newState)
+				break;
+
 			switch (event.type)
 			{
 			case SDL_QUIT:
@@ -54,47 +74,39 @@ int Engine::exec(State* entryState)
 			case SDL_KEYDOWN:
 			case SDL_MOUSEBUTTONDOWN:
 			case SDL_MOUSEBUTTONUP:
+
 				// this is the ugliest fucking thing i have ever written and i
 				// have no idea what to do with it.
+
 				newState = _state->handleActorEvent(this, event);
-				if (newState != nullptr)
-				{
-					setState(newState);
-					newState = nullptr;
+				if (newState)
 					continue;
-				}
+
 				newState = _state->handleEvent(this, event);
-				if (newState != nullptr)
-				{
-					setState(newState);
-					newState = nullptr;
+
+				// please just stop
+				if (newState)
 					continue;
-				}
+
 				break;
 			}
 		}
 
-		// what the fuck am i doing with my life
+		// i want this to stop
+		if (newState)
+			continue;
 
 		newState = _state->actorUpdate(this);
 
-		if (newState != nullptr)
-		{
-			setState(newState);
-			newState = nullptr;
+		// what the fuck am i doing with my life
+		if (newState)
 			continue;
-		}
-
-		// i can't believe i thought i was good at programming
 
 		newState = _state->update(this);
 
-		if (newState != nullptr)
-		{
-			setState(newState);
-			newState = nullptr;
+		// i can't believe i thought i was good at programming
+		if (newState)
 			continue;
-		}
 
 		// i don't believe in a god or any sort of afterlife but i do believe i
 		// am going to hell for this.
@@ -132,7 +144,7 @@ double Engine::deltaTime() const
 
 void Engine::setState(State* state)
 {
-	if (_state != nullptr)
+	if (_state)
 		_state->exit(this);
 	_state = state;
 	_state->enter(this);
